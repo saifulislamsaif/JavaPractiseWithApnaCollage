c4f059619d15fbe622e5049ac38a7f2d7dd46f25
saiful islam <saifulislambaust28@gmail.com>
2022-11-02 11:19:09 +0600

    preOrder of a tree done

--- src/com/bst/BinaryTreeYT.java
+++ src/com/bst/BinaryTreeYT.java
@@ -29,14 +29,23 @@ public class BinaryTreeYT {
 
             return newNode;
         }
-    }
 
-    public static void main(String[] args) {
+        public static void preorder(Node root){
+            if (root == null){
+                System.out.println(-1+" ");
+                return;
+            }
+            System.out.println(root.data+" ");
+            preorder(root.left);
+            preorder(root.right);
+        }
 
-        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
-        BinaryTree tree = new BinaryTree();
-        Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+        public static void main(String[] args) {
 
+            int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+            BinaryTree tree = new BinaryTree();
+            Node root = tree.buildTree(nodes);
+            preorder(root);
+        }
     }
 }
