5a808947f1e3f03b8c5505c2bb17f36a10b38cd9
saiful islam <saifulislambaust28@gmail.com>
2022-11-17 13:04:39 +0600

    apna collage start with FirstClass

---
+++ src/com/apnaCollage/FirstClass.java
@@ -0,0 +1,17 @@
+package com.apnaCollage;
+
+///              how code works
+//code hocce set of instraction jar dara amrah machine k instraction dey.
+// code decimale number k binary number e represent kore
+// machine just binary number k jane tai high level language dara amrah khub taratari binary te represent korte pari.
+
+//           how java internally works
+// sourch code k jre er majhe thaka jdk byte code e rupantor kore,
+// ja jeknno os e cole ei jonno java k wora(write one run everywhere) bole
+//then byte code k jvm machine language binary te rupantor kore executio stage e
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
