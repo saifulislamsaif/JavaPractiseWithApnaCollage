ab5b94671e8045313a1d48ad92a20e429bd6fe32
saiful <saifulremon>
2022-11-18 21:16:09 +0600

    Pattern half pyramid

--- src/com/apnaCollage/pattern/Pattern.java
+++ src/com/apnaCollage/pattern/Pattern.java
@@ -1,23 +1,22 @@
 package com.apnaCollage.pattern;
-/*      Pattern hollow rectangle
+/*      Pattern half pyramid
+
+ *
+ **
+ ***
+ ****
  *****
- *   *
- *   *
- *****
+ 
  */
 
 public class Pattern {
     public static void main(String[] args) {
         int r, c;
-        r = 4;
+        r = 5;
         c = 5;
         for (int i = 1; i <= r; i++) {
-            for (int j = 1; j <= c; j++) {
-                if (i == 1 || j == 1 || i == r || j == c) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
