77f895bc5ce6206cbc910d27b8a862f57cbe242d
saiful <saifulremon>
2022-11-18 19:52:15 +0600

    Pattern hollow rectangle code align

--- src/com/apnaCollage/pattern/Pattern.java
+++ src/com/apnaCollage/pattern/Pattern.java
@@ -1,25 +1,21 @@
 package com.apnaCollage.pattern;
 /*      Pattern hollow rectangle
-
-
  *****
  *   *
  *   *
  *****
-
-
-             */
+ */
 
 public class Pattern {
     public static void main(String[] args) {
-        int r,c;
-        r=4;
-        c=5;
-        for (int i = 1;i<=r;i++){
-            for (int j = 1;j<=c;j++){
-                if(i==1 || j==1 || i==r || j==c){
+        int r, c;
+        r = 4;
+        c = 5;
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= c; j++) {
+                if (i == 1 || j == 1 || i == r || j == c) {
                     System.out.print("*");
-                }else {
+                } else {
                     System.out.print(" ");
                 }
             }
