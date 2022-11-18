009a50ff8297552bc1eef84a39b0c8b336c5c2cc
saiful <saifulremon>
2022-11-18 19:51:14 +0600

    Pattern hollow rectangle

--- src/com/apnaCollage/pattern/Pattern.java
+++ src/com/apnaCollage/pattern/Pattern.java
@@ -1,9 +1,14 @@
 package com.apnaCollage.pattern;
-/*      Pattern Rectangle
-           *****
-           *****
-           *****
-           *****             */
+/*      Pattern hollow rectangle
+
+
+ *****
+ *   *
+ *   *
+ *****
+
+
+             */
 
 public class Pattern {
     public static void main(String[] args) {
@@ -12,7 +17,11 @@ public class Pattern {
         c=5;
         for (int i = 1;i<=r;i++){
             for (int j = 1;j<=c;j++){
-                System.out.print("*");
+                if(i==1 || j==1 || i==r || j==c){
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
