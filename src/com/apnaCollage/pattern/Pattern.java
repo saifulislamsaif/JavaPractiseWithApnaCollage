6a2ecfff71c4630130dd09f4016c04943b979b63
saiful <saifulremon>
2022-11-20 07:46:53 +0600

    half pyramid with number

--- src/com/apnaCollage/pattern/Pattern.java
+++ src/com/apnaCollage/pattern/Pattern.java
@@ -13,11 +13,8 @@ public class Pattern {
         int n;
         n = 4;
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print(j);
             }
             System.out.println();
         }
