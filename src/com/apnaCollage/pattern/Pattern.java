415f9599dd87f6984b71b3b2cc78a6bae51c8780
saiful <saifulremon>
2022-11-20 08:00:32 +0600

    Floyd's triangle with numbers

--- src/com/apnaCollage/pattern/Pattern.java
+++ src/com/apnaCollage/pattern/Pattern.java
@@ -1,19 +1,20 @@
 package com.apnaCollage.pattern;
-/*       inverted half pyramid with numbers
+/*       Floyd's triangle with numbers
 
-1 2 3 4
-1 2 3
-1 2
 1
+2 3
+4 5 6
+7 8 9 10
  */
 
 public class Pattern {
     public static void main(String[] args) {
-        int n;
+        int n,number = 1;
         n = 4;
-        for (int i = n; i >=1; i--) {
+        for (int i = 0; i <=n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(j+" ");
+                System.out.print(number+" ");
+                number++;
             }
             System.out.println();
         }
