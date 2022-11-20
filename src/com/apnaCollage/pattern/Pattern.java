838a2f80321ad3725c621742f8e9cc094606d024
saiful islam <saifulislambaust28@gmail.com>
2022-11-20 12:01:48 +0600

    slid rhombus with stars

--- src/com/apnaCollage/pattern/Pattern.java
+++ src/com/apnaCollage/pattern/Pattern.java
@@ -1,52 +1,28 @@
 package com.apnaCollage.pattern;
-/*       Butterfly pattern with stars
+/*       slid rhombus with stars
 
- *      *
- **    **
- ***  ***
- ********
- ********
- ***  ***
- **    **
- *      *
+ ******
+ ******
+ ******
+ ******
+ ******
 
  */
 
 public class Pattern {
     public static void main(String[] args) {
         int n;
-        n = 4;
-        for (int i = 1; i <= n; i++) {
-//            1st half right portion
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            //space print
-            int space = 2 * (n - i);
-            for (int j = 1; j <= space; j++) {
+        n = 5;
+
+        for (int i=1;i<=n;i++){
+            for (int j = 0; j<=n-i;j++){
                 System.out.print(" ");
             }
-//            1st half right portion
-            for (int j = 1; j <= i; j++) {
+            for (int j = 0;j<=n;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
-        for (int i = n; i >= 1; i--) {
-//            2nd half left portion
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-//            2nd half space portion
-            int space = 2 * (n - i);
-            for (int j = 1; j <= space; j++) {
-                System.out.print(" ");
-            }
-//            2nd half right portion
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+
     }
 }
