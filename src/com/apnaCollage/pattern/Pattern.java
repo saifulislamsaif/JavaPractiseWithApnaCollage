5c855c7dd4e7d0da6f3eda1e3eb95cb852864a18
saiful <saifulremon>
2022-11-20 07:53:40 +0600

    inverted half pyramid with numbers

--- src/com/apnaCollage/pattern/Pattern.java
+++ src/com/apnaCollage/pattern/Pattern.java
@@ -1,18 +1,17 @@
 package com.apnaCollage.pattern;
-/*        half pyramid with numbers
+/*       inverted half pyramid with numbers
 
-
-1
-1 2
-1 2 3
 1 2 3 4
+1 2 3
+1 2
+1
  */
 
 public class Pattern {
     public static void main(String[] args) {
         int n;
         n = 4;
-        for (int i = 1; i <= n; i++) {
+        for (int i = n; i >=1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j+" ");
             }
