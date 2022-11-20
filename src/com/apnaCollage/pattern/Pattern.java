e76835901941d553762028bf2fa8bb2b87dd4288
saiful islam <saifulislambaust28@gmail.com>
2022-11-20 12:02:07 +0600

    slid rhombus with stars

--- src/com/apnaCollage/pattern/Pattern.java
+++ src/com/apnaCollage/pattern/Pattern.java
@@ -14,11 +14,11 @@ public class Pattern {
         int n;
         n = 5;
 
-        for (int i=1;i<=n;i++){
-            for (int j = 0; j<=n-i;j++){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 0;j<=n;j++){
+            for (int j = 0; j <= n; j++) {
                 System.out.print("*");
             }
             System.out.println();
