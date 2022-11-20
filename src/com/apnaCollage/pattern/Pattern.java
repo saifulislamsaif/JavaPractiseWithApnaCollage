97e1fc78cee81071b4c2b54a91680818e486e793
saiful islam <saifulislambaust28@gmail.com>
2022-11-20 11:50:15 +0600

    Butterfly pattern with stars

--- src/com/apnaCollage/pattern/Pattern.java
+++ src/com/apnaCollage/pattern/Pattern.java
@@ -1,25 +1,50 @@
 package com.apnaCollage.pattern;
-/*       0-1 triangle
+/*       Butterfly pattern with stars
 
-1
-0 1
-1 0 1
-0 1 0 1
+ *      *
+ **    **
+ ***  ***
+ ********
+ ********
+ ***  ***
+ **    **
+ *      *
+
  */
 
 public class Pattern {
     public static void main(String[] args) {
         int n;
         n = 4;
-        for (int i = 0; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
+//            1st half right portion
             for (int j = 1; j <= i; j++) {
-                int sum = i + j;
-                if (sum % 2 == 0) {
-                    System.out.print("1 ");
-                } else {
-                    System.out.print("0 ");
-                }
-
+                System.out.print("*");
+            }
+            //space print
+            int space = 2 * (n - i);
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+//            1st half right portion
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+//            2nd half left portion
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+//            2nd half space portion
+            int space = 2 * (n - i);
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+//            2nd half right portion
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
