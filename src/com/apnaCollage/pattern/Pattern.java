6f2c248e5d8a9126eb0bdcf095b5296cf365aad5
saiful islam <saifulislambaust28@gmail.com>
2022-11-20 12:43:45 +0600

    diamond stars

--- src/com/apnaCollage/pattern/Pattern.java
+++ src/com/apnaCollage/pattern/Pattern.java
@@ -1,26 +1,38 @@
 package com.apnaCollage.pattern;
-/*       number pyramid
+/*       diamond stars
 
-    1
-   2 2
-  3 3 3
- 4 4 4 4
-5 5 5 5 5
-
+    *
+   ***
+  *****
+ *******
+ *******
+  *****
+   ***
+    *
 
  */
 
 public class Pattern {
     public static void main(String[] args) {
         int n;
-        n = 5;
+        n = 4;
 
         for (int i = 1; i <= n; i++) {
             for (int j=1;j<=n-i;j++){
                 System.out.print(" ");
             }
-            for (int j=1;j<=i;j++){
-                System.out.print(i+" ");
+            for (int j=1;j<=(2*i-1);j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n; i >=1; i--) {
+            for (int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for (int j=1;j<=(2*i-1);j++){
+                System.out.print("*");
             }
             System.out.println();
         }
