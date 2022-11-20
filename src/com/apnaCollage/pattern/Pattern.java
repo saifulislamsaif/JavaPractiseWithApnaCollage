4d13ab5f9cb7fd4b12cd496a3b338a83728e27a1
saiful <saifulremon>
2022-11-20 07:43:36 +0600

    inverted half pyramid (rotated 180^0 pyramid)

--- src/com/apnaCollage/pattern/Pattern.java
+++ src/com/apnaCollage/pattern/Pattern.java
@@ -1,20 +1,21 @@
 package com.apnaCollage.pattern;
 /*       inverted half pyramid
+         (rotated 180^0 pyramid)
 
- *****
- ****
- ***
- **
- *
-
+       *
+      **
+     ***
+    ****
  */
 
 public class Pattern {
     public static void main(String[] args) {
-        int r, c;
-        r = 5;
-        c = 5;
-        for (int i = r; i >= 1; i--) {
+        int n;
+        n = 4;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
