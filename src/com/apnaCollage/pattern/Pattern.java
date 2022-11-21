de652524635f9658b79a382ef8ed00d3eb2e1102
saiful islam <saifulislambaust28@gmail.com>
2022-11-21 13:34:13 +0600

    Print a hollow Butterfly.

--- src/com/apnaCollage/pattern/Pattern.java
+++ src/com/apnaCollage/pattern/Pattern.java
@@ -1,25 +1,59 @@
 package com.apnaCollage.pattern;
-/*       Print a hollow Rhombus
+/*       Print a hollow Butterfly.
 
-      *****
-     *   *
-   *   *
-  *   *
- *****
+ *        *
+ **      **
+ * *    * *
+ *  *  *  *
+ *   **   *
+ *   **   *
+ *  *  *  *
+ * *    * *
+ **      **
+ *        *
 
  */
 
 public class Pattern {
     public static void main(String[] args) {
-        int i,j,n = 5;
-        for(i =1; i<=n; i++){
-            for (j=1;j<=n-i;j++){
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (i == j || j == 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
                 System.out.print(" ");
             }
-            for (j=1;j<=n;j++){
-                if (i==1 || i==n || j==1 || j==n){
+            for (int j = 1; j <= i; j++) {
+                if (i == j || j == 1) {
                     System.out.print("*");
-                }else {
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                if (i == j || j == 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                if (i == j || j == 1) {
+                    System.out.print("*");
+                } else {
                     System.out.print(" ");
                 }
             }
