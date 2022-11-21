e732368cc6fb8bb5c5fa39911f2d7e0175e546d8
saiful islam <saifulislambaust28@gmail.com>
2022-11-21 12:29:05 +0600

    Print a hollow Rhombus

--- src/com/apnaCollage/pattern/Pattern.java
+++ src/com/apnaCollage/pattern/Pattern.java
@@ -1,41 +1,31 @@
 package com.apnaCollage.pattern;
-/*       diamond stars
+/*       Print a hollow Rhombus
 
-    *
-   ***
-  *****
- *******
- *******
-  *****
-   ***
-    *
+      *****
+     *   *
+   *   *
+  *   *
+ *****
 
  */
 
 public class Pattern {
     public static void main(String[] args) {
-        int n;
-        n = 4;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j=1;j<=n-i;j++){
+        int i,j,n = 5;
+        for(i =1; i<=n; i++){
+            for (j=1;j<=n-i;j++){
                 System.out.print(" ");
             }
-            for (int j=1;j<=(2*i-1);j++){
-                System.out.print("*");
+            for (j=1;j<=n;j++){
+                if (i==1 || i==n || j==1 || j==n){
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
 
-        for (int i = n; i >=1; i--) {
-            for (int j=1;j<=n-i;j++){
-                System.out.print(" ");
-            }
-            for (int j=1;j<=(2*i-1);j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
 
     }
 }
