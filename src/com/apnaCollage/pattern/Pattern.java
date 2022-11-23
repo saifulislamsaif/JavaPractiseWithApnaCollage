1fec1a4e9226173d4d52b9c0d288f1e07ce8a6af
saiful islam <saifulislambaust28@gmail.com>
2022-11-23 12:38:51 +0600

    infinite do while loop condition

--- src/com/apnaCollage/pattern/Pattern.java
+++ src/com/apnaCollage/pattern/Pattern.java
@@ -1,30 +1,12 @@
 package com.apnaCollage.pattern;
 
-import java.util.Scanner;
-
 public class Pattern {
-    public String eligibleToVote(int a) {
-        String s = "This Person Eligible for Vote";
-        String n = "This Person is not Eligible for Vote";
-        if (a >= 18) {
-            return s;
-        } else
-            return n;
-    }
-
+    
     public static void main(String args[]) {
-        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Enter First Number");
-//        int firstN = scanner.nextInt();
-//        System.out.println("Enter First Number");
-//        int secondN = scanner.nextInt();
-        System.out.println("Enter the Age:");
-        int age = scanner.nextInt();
 
-        Pattern pattern = new Pattern();
-        String result = pattern.eligibleToVote(age);
-        System.out.println(result);
-
+        do {
+            System.out.println("it is infinite loop");
+        } while (true);
     }
 }
 
