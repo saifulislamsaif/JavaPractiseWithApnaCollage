e6279047c20759939e3cd7f5c129441b3f130d3f
saiful islam <saifulislambaust28@gmail.com>
2022-11-23 17:57:21 +0600

    soome code added

--- src/com/apnaCollage/pattern/Pattern.java
+++ src/com/apnaCollage/pattern/Pattern.java
@@ -1,12 +1,36 @@
 package com.apnaCollage.pattern;
 
-public class Pattern {
-    
-    public static void main(String args[]) {
+import java.util.Scanner;
 
-        do {
-            System.out.println("it is infinite loop");
-        } while (true);
+public class Pattern {
+    public void eligibleToVote(int a) {
+        int zero=0;
+        int positive=0;
+        int negative=0;
+     for (int i = 0;i<=a;i++){
+         if (i >= 1 ){
+             positive++;
+         }else if ( i < 1){
+             negative++;
+         }else {
+             zero++;
+         }
+     }
+        System.out.println("Number of positive is"+" "+positive +" "+"Number of negative is"+" "+negative +" "+"Number of positive is"+" "+zero +" ");
+    }
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter First Number");
+//        int firstN = scanner.nextInt();
+//        System.out.println("Enter First Number");
+//        int secondN = scanner.nextInt();
+        System.out.println("Enter the Number:");
+        int number = scanner.nextInt();
+
+        Pattern pattern = new Pattern();
+        pattern.eligibleToVote(number);
+
     }
 }
 
