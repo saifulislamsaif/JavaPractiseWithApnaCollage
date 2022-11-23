6ed4e645b31da5cd02f11b74c62d1458f1e46394
saiful islam <saifulislambaust28@gmail.com>
2022-11-23 12:08:55 +0600

    Greater number of two number

--- src/com/apnaCollage/pattern/Pattern.java
+++ src/com/apnaCollage/pattern/Pattern.java
@@ -12,24 +12,24 @@ import java.time.Period;
 import java.util.Scanner;
 
 public class Pattern {
-int n=5,sum=0;
-    public void sum(){
-        for (int i = 0;i<=n;i++){
-            if (i%2==1){
-                sum=sum+i;
-            }
-
+    public void max(int first,int second){
+        if (first>second){
+            System.out.println("Big number is "+" "+ first);
+        }else if(first==second) {
+            System.out.println("Two number are equal");
+        }else {
+            System.out.println("Big number is "+" "+ second);
         }
-        System.out.println(sum);
     }
     public static void main(String args[]) {
-//        Scanner scanner = new Scanner(System.in);
-//        int firstN = scanner.nextInt();
-//        int secondN = scanner.nextInt();
-//        int thirdN = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First Number");
+        int firstN = scanner.nextInt();
+        System.out.println("Enter First Number");
+        int secondN = scanner.nextInt();
 
         Pattern pattern = new Pattern();
-        pattern.sum();
+        pattern.max(firstN,secondN);
 
     }
 }
