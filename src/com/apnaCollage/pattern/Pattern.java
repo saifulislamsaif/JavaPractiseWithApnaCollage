635e6dd47f300b27e522199cddb6c02960e4f40a
saiful islam <saifulislambaust28@gmail.com>
2022-11-23 12:31:25 +0600

    eligibility to vote.

--- src/com/apnaCollage/pattern/Pattern.java
+++ src/com/apnaCollage/pattern/Pattern.java
@@ -1,33 +1,29 @@
 package com.apnaCollage.pattern;
-/*       Print Inverted Half Pyramid
 
-1111
-222
-33
-4
-
- */
-
-import java.time.Period;
 import java.util.Scanner;
 
 public class Pattern {
-   Double pi =3.1416;
-    public void max(int r){
-        Double radius = 2*pi*r;
-        System.out.println("Circumference is"+" "+radius);
+    public String eligibleToVote(int a) {
+        String s = "This Person Eligible for Vote";
+        String n = "This Person is not Eligible for Vote";
+        if (a >= 18) {
+            return s;
+        } else
+            return n;
     }
+
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
 //        System.out.println("Enter First Number");
 //        int firstN = scanner.nextInt();
 //        System.out.println("Enter First Number");
 //        int secondN = scanner.nextInt();
-        System.out.println("Enter the Radius:");
-        int radius = scanner.nextInt();
+        System.out.println("Enter the Age:");
+        int age = scanner.nextInt();
 
         Pattern pattern = new Pattern();
-        pattern.max(radius);
+        String result = pattern.eligibleToVote(age);
+        System.out.println(result);
 
     }
 }
